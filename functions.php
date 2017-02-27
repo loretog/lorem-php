380227e9861a0865fcf62e1a312422cc647e9f5c
Loreto Gabawa Jr <jun.rej@gmail.com>
2017-02-27 17:06:59 +0800

    update

--- functions.php
+++ functions.php
@@ -2,6 +2,9 @@
 
 if( ! defined( 'ACCESS' ) ) die( 'DIRECT ACCESS NOT ALLOWED' );
 
-function element( $el ) {
-	include "./elements/$el.php";
+function element( $el ) {	
+	include ROOT_DIR . "/elements/$el.php";
 }
+
+
+/* ADD YOUR CUSTOM FUNCTIONS BELOW */
