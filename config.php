ca6b2b3a774394c47f466b74664c698795b0d3ec
Loreto Gabawa Jr <jun.rej@gmail.com>
2018-03-12 15:08:05 +0800

    update

--- config.php
+++ config.php
@@ -16,4 +16,12 @@ IF NOT THEN JUST COMMENT THEM OUT
 define( 'AUTH_ID', 'userid' );
 define( 'AUTH_NAME', 'username' );
 define( 'AUTH_TYPE', 'usertype' );
+
+/*
+	TO USE:
+		To add restricted pages, just uncomment the variable $restricted_pages,
+		each array elements are page names found in your pages folder.
+		When added, these pages will not be accessible unless the SESSION AUTH_ID
+		is assigned with a value.
+*/
 //$restricted_pages = array( "payments", "shop", "users", "products" );
