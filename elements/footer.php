50cdb1225b325a9d5e450a0f453ad394b11d477f
Loreto Gabawa Jr <loreto@nonibrands.com>
2021-12-07 10:15:43 +0800

    update

--- elements/footer.php
+++ elements/footer.php
@@ -1,26 +1,12 @@
 <?php if( ! defined( 'ACCESS' ) ) die( 'DIRECT ACCESS NOT ALLOWED' ); ?>
 				<!-- END OF CONTENT -->
 				</div>
-				<div class="col-lg-4 col-md-4">
-					<div class="list-group">
-					  <a href="#" class="list-group-item active">
-					    Cras justo odio
-					  </a>
-					  <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
-					  <a href="#" class="list-group-item">Morbi leo risus</a>
-					  <a href="#" class="list-group-item">Porta ac consectetur ac</a>
-					  <a href="#" class="list-group-item">Vestibulum at eros</a>
-					</div>
-				</div>
 			</div>
 			<hr>
 			<div class="footer col-lg-12">
-				<a href="./">Home</a>
-				<a href="./?page=products">Products</a>
+				<a href="<?php echo SITE_URL ?>">Home</a>			
 			</div>
-		</div>
-
-		<script type="text/javascript" src="<?php echo SITE_URL ?>/assets/bootstrap/js/jquery-2.1.3.min.js"></script>
+		</div>		
 		<script type="text/javascript" src="<?php echo SITE_URL ?>/assets/bootstrap/js/bootstrap.min.js"></script>
 	</body>
 </html>
--- elements/header.php
+++ elements/header.php
@@ -2,75 +2,40 @@
 <!DOCTYPE html> 
 <html>
 	<head>
-		<title>Awesome Site</title>
+		<title>Lorem Framework</title>
 		<link rel="stylesheet" type="text/css" href="<?php echo SITE_URL ?>/assets/bootstrap/css/bootstrap.min.css">
 	</head>
 
 	<body>
-
+		<?php 
+			global $restricted_pages;
+		?>
 		<div class="container">
 			<div class="row">
+
+				
+
 				<div class="header col-lg-12">
 					<div class="page-header">
-					  <h1>My Awesome Site <small>Subtext for header</small></h1>
+					  <h1>Lorem Framework <small></small></h1>
 					</div>				
 				</div>
+
 				<div class="menu col-lg-12">
-					<nav class="navbar navbar-default">
-					  <div class="container-fluid">					   
-					  	<!-- Brand and toggle get grouped for better mobile display -->
-					    <div class="navbar-header">
-					      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-					        <span class="sr-only">Toggle navigation</span>
-					        <span class="icon-bar"></span>
-					        <span class="icon-bar"></span>
-					        <span class="icon-bar"></span>
-					      </button>
-					      <a class="navbar-brand" href="#">Brand</a>
-					    </div>
-					    <!-- Collect the nav links, forms, and other content for toggling -->
-					    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					      <ul class="nav navbar-nav">
-					        <li class="active"><a href="./">Home <span class="sr-only">(current)</span></a></li>
-					        <li><a href="./?page=products">Products</a></li>
-					        <li class="dropdown">
-					          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
-					          <ul class="dropdown-menu" role="menu">
-					            <li><a href="#">Action</a></li>
-					            <li><a href="#">Another action</a></li>
-					            <li><a href="#">Something else here</a></li>
-					            <li class="divider"></li>
-					            <li><a href="#">Separated link</a></li>
-					            <li class="divider"></li>
-					            <li><a href="#">One more separated link</a></li>
-					          </ul>
-					        </li>					        
-					        <?php if( isset( $_SESSION[ AUTH_ID ] ) ) { ?>
-					        <li><a href="./?action=logout">Logout</a></li>
-					        <?php } ?>
-					      </ul>					     
-					      <ul class="nav navbar-nav navbar-right">
-					        <li><a href="#">Link</a></li>
-					        <li class="dropdown">
-					          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
-					          <ul class="dropdown-menu" role="menu">
-					            <li><a href="#">Action</a></li>
-					            <li><a href="#">Another action</a></li>
-					            <li><a href="#">Something else here</a></li>
-					            <li class="divider"></li>
-					            <li><a href="#">Separated link</a></li>
-					          </ul>
-					        </li>
+					<nav class="navbar navbar-expand-lg navbar-light bg-light">
+					  <div class="container-fluid">				    
+					    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+					      <ul class="navbar-nav me-auto mb-2 mb-lg-0">					        
+									<li class="nav-item"><a class="nav-link" href="<?php echo SITE_URL ?>">Home</a></li>					        
 					      </ul>
-					    </div><!-- /.navbar-collapse -->
-					  </div><!-- /.container-fluid -->
+					      
+					    </div>
+					  </div>
 					</nav>
 				</div>
 			</div>
 			<?php 
-				if( !empty( $MESSAGE ) ) {
-					echo "<div class='row'><div class='col-lg-12'><div class='alert alert-$MESSAGE_TYPE'>$MESSAGE</div></div></div>";
-				} 
+				show_message();
 			?>
 			<div class="main-content row">				
 				<div class="col-lg-8 col-md-8">
