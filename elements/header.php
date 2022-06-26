7546ebcc783cd86854912f0eadd85e2a786c17b7
Loreto Gabawa Jr <jun.rej@gmail.com>
2022-06-26 08:44:43 +0800

    update

--- elements/header.php
+++ elements/header.php
@@ -8,52 +8,22 @@
     <title><?= SITE_TITLE ?></title>
     <link href="<?= SITE_URL ?>/assets/bootstrap-5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="<?= SITE_URL ?>/assets/bootstrap-icons-1.8.3/bootstrap-icons.css">
-    <link rel="stylesheet" href="<?= SITE_URL ?>/assets/css/style.css">
+    <link rel="stylesheet" href="<?= SITE_URL ?>/assets/css/style.css?a=<?= time() ?>">
     <script>
       var $siteUrl = "<?= SITE_URL ?>";  
     </script>
   </head>
   <body>    
     <div class="container-fluid">
-      <div class="row header" style="background-color: #d0c971;">
+      <div class="row header">
         <div class="col">
-          <ul class="nav nav-pills p-2">
+          <ul class="nav nav-pills p-2 bg-light">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="<?= SITE_URL ?>">TRADEAN</a>
+              <a class="nav-link active" aria-current="page" href="<?= SITE_URL ?>"><?= SITE_TITLE ?></a>
             </li>            
           </ul>
         </div>
       </div>      
-      <div class="row">
-      <div class="col-2">
-        <div class="list-group">
-          <a href="<?= SITE_URL ?>/" class="list-group-item list-group-item-action <?= !isset($_GET[ 'page' ]) || empty($_GET[ 'page' ]) || $_GET[ 'page' ] == "" ? 'active' : '' ?>" aria-current="true">
-            Dashboard
-          </a>
-          <a href="<?= SITE_URL ?>/?page=to-do-list" class="list-group-item list-group-item-action <?= isset($_GET[ 'page' ]) && $_GET[ 'page' ] == "to-do-list" ? 'active' : '' ?>">To-Do List</a>
-          <a href="<?= SITE_URL ?>/?page=topics" class="list-group-item list-group-item-action <?= isset($_GET[ 'page' ]) && $_GET[ 'page' ] == "topics" ? 'active' : '' ?>">Topics</a>
-          <a href="<?= SITE_URL ?>/?page=activities" class="list-group-item list-group-item-action <?= isset($_GET[ 'page' ]) && ($_GET[ 'page' ] == "activities" || $_GET[ 'page' ] == "view-activity" || $_GET[ 'page' ] == "new-activity-file" ) ? 
-          'active' : '' ?>">Activities</a>
-          <?php if( is_usertype( "professor" ) ) { ?>          		
-          <a href="<?= SITE_URL ?>/?page=students" class="list-group-item list-group-item-action <?= isset($_GET[ 'page' ]) && ($_GET[ 'page' ] == "students" ) ? 
-          'active' : '' ?>">Students</a>
-          <a href="<?= SITE_URL ?>/?page=professors" class="list-group-item list-group-item-action <?= isset($_GET[ 'page' ]) && ($_GET[ 'page' ] == "professors" || $_GET[ 'page' ] == "new-professor" ) ? 
-          'active' : '' ?>">Professors</a>
-          <?php } ?>
-
-          <a href="<?= SITE_URL ?>/?page=profile" class="list-group-item list-group-item-action <?= isset($_GET[ 'page' ]) && ($_GET[ 'page' ] == "profile" ) ? 
-          'active' : '' ?>">Profile</a>		
-
-          <?php if( is_usertype( "professor" ) ) { ?>          
-          <?php } else { ?>          
-          <?php } ?>
-          <a href="<?= SITE_URL ?>/?action=logout" class="list-group-item list-group-item-action">Logout</a>
-
-        </div>
-        <div class="m-2 text-light">
-          Welcome back <span class="fw-bold text-uppercase"><?= $_SESSION[ AUTH_NAME ] ?></span><br>
-          Logged in as <span class="fw-bold text-uppercase"><?= $_SESSION[ AUTH_TYPE ] ?></span>
-        </div>        
-      </div>
+      <div class="row">      
           <div class="col left-content">
           <?= show_message(); ?>
--- pages/login.php
+++ pages/login.php
@@ -1,33 +1,6 @@
 <?php if( ! defined( 'ACCESS' ) ) die( 'DIRECT ACCESS NOT ALLOWED' ); ?>
 
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?= SITE_TITLE ?></title>
-    <link href="<?= SITE_URL ?>/assets/bootstrap-5.2/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?= SITE_URL ?>/assets/bootstrap-icons-1.8.3/bootstrap-icons.css">
-    <link rel="stylesheet" href="<?= SITE_URL ?>/assets/css/style.css">
-    <script>
-      var $siteUrl = "<?= SITE_URL ?>";  
-    </script>
-  </head>
-  <body>    
-    <div class="container-fluid">
-      <div class="row header" style="background-color: #d0c971;">
-        <div class="col">
-          <ul class="nav nav-pills p-2">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="<?= SITE_URL ?>">TRADEAN</a>
-            </li>            
-          </ul>
-        </div>
-      </div>      
-      <div class="row main-content p-2">      
-          <div class="col-4 left-content">
-          <?= show_message(); ?>
-
+<?= element( 'header' ) ?>
 <?= element( 'login_form' ) ?>
 <?= element( 'footer' ) ?>
         
