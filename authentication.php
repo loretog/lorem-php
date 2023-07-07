22f03a737df04c786375e87771d26d2d8004e028
mycodesgit <112681686+mycodesgit@users.noreply.github.com>
2023-07-07 22:50:52 +0800

    Update authentication.php

--- authentication.php
+++ authentication.php
@@ -1,36 +1,58 @@
 <?php	
-	if( ! defined( 'ACCESS' ) ) die( 'DIRECT ACCESS NOT ALLOWED' );
 
-	/* 
-	NOTE:
-	ONLY SET THESE IF YOU WANT TO ALLOW AUTHENTICATION 
-	IF NOT THEN JUST COMMENT THEM OUT 
-	*/
+/*
+|--------------------------------------------------------------------------
+| Authentication
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all the pages you want to allow for your web application. These
+| page authentication giving you access in all "pages" which
+| depends in every "Usertype". Now create more authentication pages!
+|
+*/
 
-	// table columns found in your 'users' table
-	define( 'AUTH_ID', 'userid' );
-	define( 'AUTH_EMAIL', 'email' );
-	define( 'AUTH_NAME', 'username' );
-	define( 'AUTH_TYPE', 'usertype' );
+/*
+|--------------------------------------------------------------------------
+| TO USE:
+|--------------------------------------------------------------------------
+|
+| To add restricted pages, just uncomment the variable $restricted_pages,
+| each array elements are page names found in your pages folder.
+| When added, these pages will not be accessible unless the SESSION AUTH_ID
+| is assigned with a value.
+|
+*/
 
-	// default page to login, name of the file found in /pages
-	define( 'LOGIN_REDIRECT', 'login' ); 
+/*
+|--------------------------------------------------------------------------
+| NOTE:
+|--------------------------------------------------------------------------
+|
+| ONLY SET THESE IF YOU WANT TO ALLOW AUTHENTICATION. IF NOT THEN JUST COMMENT THEM OUT
+|
+*/
 
-	/*
-		TO USE:
-			To add restricted pages, just uncomment the variable $restricted_pages,
-			each array elements are page names found in your pages folder.
-			When added, these pages will not be accessible unless the SESSION AUTH_ID
-			is assigned with a value.
-	*/
 
-	/* $restricted_pages[ 'admin' ]['access'] = [ "admin-page" ];
-	$restricted_pages[ 'admin' ][ 'default_page' ] = "default";
+if( ! defined( 'ACCESS' ) ) die( 'DIRECT ACCESS NOT ALLOWED' );
 
-	$restricted_pages[ 'user' ]['access'] = [ "user-page" ];
-	$restricted_pages[ 'user' ][ 'default_page' ] = "default";
-	*/
-	$restricted_pages[ 'default' ]['access'] = [ "login", "register" ];
-	$restricted_pages[ 'default' ][ 'default_page' ] = "login"; 
+// table columns found in your 'users' table
+define( 'AUTH_ID', 'id' );
+define( 'AUTH_NAME', 'username' );
+define( 'AUTH_TYPE', 'usertype' );
+define( 'AUTH_TOKEN', 'token' );
 
-	has_access( true );
+// default page to login, name of the file found in /pages
+define( 'LOGIN_REDIRECT', 'default' ); 
+
+
+
+/* $restricted_pages[ 'admin' ]['access'] = [ "admin-page" ];
+$restricted_pages[ 'admin' ][ 'default_page' ] = "default";
+
+$restricted_pages[ 'user' ]['access'] = [ "user-page" ];
+$restricted_pages[ 'user' ][ 'default_page' ] = "default";
+*/
+$restricted_pages[ 'default' ]['access'] = [ "default", "login", "register" ];
+$restricted_pages[ 'default' ][ 'default_page' ] = "default"; 
+
+has_access( true );
