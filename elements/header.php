f877bb6ad071f212f9d32dc62750e9c08176847b
mycodesgit <112681686+mycodesgit@users.noreply.github.com>
2023-07-08 00:06:09 +0800

    Update header.php

--- elements/header.php
+++ elements/header.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= SITE_TITLE ?></title>
     <link href="<?= SITE_URL ?>/assets/bootstrap-5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="<?= SITE_URL ?>/assets/bootstrap-icons-1.8.3/bootstrap-icons.css">
-    <link rel="stylesheet" href="<?= SITE_URL ?>/assets/css/style.css?a=<?= time() ?>">
+    <link rel="stylesheet" href="<?php echo dirname($_SERVER['PHP_SELF']); ?>/assets/bootstrap-icons-1.8.3/bootstrap-icons.css">
+    <link rel="stylesheet" href="<?php echo dirname($_SERVER['PHP_SELF']); ?>/assets/css/style.css?a=<?= time() ?>">
     <script>
       var $siteUrl = "<?= SITE_URL ?>";  
     </script>
